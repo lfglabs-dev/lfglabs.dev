6033905b3dec2510860841b818f134713ee5779d
Thomas Marchand <thomas.marchand@tuta.io>
2024-09-29 09:48:25 +0100

    feat: dynamic favicon

--- pages/_document.js
+++ pages/_document.js
@@ -2,11 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'LFG Labs',
+    description:
+      'Web3-focused team of builders creating user-friendly products, from design to code.',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
-
   return (
     <Html lang="en">
       <Head>
@@ -21,6 +21,7 @@ export default function Document() {
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />{' '}
       </Head>
       <body>
         <Main />
