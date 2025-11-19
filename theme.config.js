92df6c554eeb8efa952a63da7f52c6b993c4b86a
Fricoben <benjamin.flores.pro@gmail.com>
2025-11-19 09:10:51 +0000

    ref: add backlink to relens

--- theme.config.js
+++ theme.config.js
@@ -3,18 +3,10 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © LFG LABS.
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
-          }
-        }
-      `}</style>
+      <time>{YEAR}</time> © LFG LABS. <br /> Powered by{' '}
+      <span><a href="https://relens.ai" target="_blank" rel="noopener noreferrer">
+        ReLens AI</a></span>
+
     </small>
   ),
   darkMode: true,
